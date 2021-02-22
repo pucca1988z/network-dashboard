2ed27cbf12c3d5fe28585ed58e59b141dfefb6e2
shepherd.fan <pucca1988z@gmail.com>
2021-02-23 00:49:12 +0800

    front-end: download static 80%; data bind is not ready

---
+++ src/components/Dashboard/DownloadStatic/DownloadTable.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="py-2 px-4 flex flex-col content-center ">
+    <div class="flex flex-row  border-gray-400 border bg-indigo-400 font-bold text-center">
+      <div class="w-20 border  border-gray-400">排名</div>
+      <div class="w-80 border  border-gray-400">IP</div>
+      <div class="w-40 border  border-gray-400">下載流量</div>
+      <div class="w-40 border  border-gray-400">國家</div>
+      <div class="flex-1 border  border-gray-400">備註</div>
+    </div>
+    <div 
+      class="border-gray-400 border-4 h-36 border-dashed text-center pt-14 text-gray-400 text-3xl font-extrabold"
+    >
+      尚無資料
+    </div>
+    <!-- <div class="flex flex-row  border-gray-400 border-b ">
+      <div class="w-20 border  border-gray-400 px-2">縣市</div>
+      <div class="w-80 border  border-gray-400 px-2">行政區</div>
+      <div class="w-40 border  border-gray-400 px-2">單位</div>
+      <div class="w-40 border  border-gray-400 px-2">事件類別</div>
+      <div class="flex-1 border  border-gray-400 px-2">事件類別</div>
+    </div>
+     -->
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
